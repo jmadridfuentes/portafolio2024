d395a13216d0d452dc13aa66bbdefec1ca4ff51b
jmadridfuentes <jmadridfuentes@gmail.com>
2024-08-26 19:49:17 -0400

    add

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+    // Bienvenida
+    alert ("¡Hola! Soy Juan, Bienvenido a mi Portafolio, donde podrá ver mis trabajos y servicios profesionales que puedo ofrecer. Aquí podrá encontrar toda la información que busca sobre mi.\nSi necesitas me escribes un mensaje al contacto");
+  
